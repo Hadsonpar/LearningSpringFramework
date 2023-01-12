53bd635b00b321ac06a0123ff37f528d3300943e
Hadsonpar <hadson1@gmail.com>
2023-01-11 23:46:05 -0500

    Demo Thymeleaf and Spring Boot

---
+++ thymeleaf-spring-boot/src/main/java/com/hadsonpar/thymeleaf/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.hadsonpar.thymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+}
