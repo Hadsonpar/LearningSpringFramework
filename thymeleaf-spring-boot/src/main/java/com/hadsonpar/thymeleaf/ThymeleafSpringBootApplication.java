135fe3f6553135669d1947a5f54596e77c21de44
Hadsonpar <hadson1@gmail.com>
2023-01-17 00:42:19 -0500

    Demo Thymeleaf and Spring Boot (rama main)

---
+++ thymeleaf-spring-boot/src/main/java/com/hadsonpar/thymeleaf/ThymeleafSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hadsonpar.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringBootApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-spring-boot/src/test/java/com/hadsonpar/thymeleaf/ThymeleafSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadsonpar.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
