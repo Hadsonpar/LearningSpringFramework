a64da6ed78cc24032b2d8db7632b68b720a4145b
Hadsonpar <hadson1@gmail.com>
2023-01-17 00:49:14 -0500

    Creando proyectos con Spring Initializr - demo03

---
+++ spring-initializr/src/main/java/com/hadsonpar/springinitializr/SpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.hadsonpar.springinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrApplication.class, args);
+	}
+
+}
---
+++ spring-initializr/src/test/java/com/hadsonpar/springinitializr/SpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadsonpar.springinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
